be0abc7a658d6865a922e034955a147a294daaba
Locus_Natit <3113135718@qq.com>
2021-08-06 12:32:05 +0800

    Total Progressing XX(add in some localize & modify textures)...

--- src/main/java/lnatit/mcardsth/handler/CardEntitySpawn.java
+++ src/main/java/lnatit/mcardsth/handler/CardEntitySpawn.java
@@ -50,6 +50,9 @@ public class CardEntitySpawn
                     }
                 }
             }
+        } else if (entity instanceof CardEntity)
+        {
+            entity.setCustomNameVisible(true);
         }
     }
 }
